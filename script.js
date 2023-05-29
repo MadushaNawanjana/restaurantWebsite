afe3eb84062dd3e0b99c8f89987d8934a2b70975
Madusha Nawanjana <95086533+MadushaNawanjana@users.noreply.github.com>
2023-05-29 23:00:37 +0530

    third commit

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-let section = document.querySelector('section');
-let navLinks = document.querySelector('header .navbar a');
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () => {
+      
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 
@@ -23,9 +24,9 @@ window.onscroll = () => {
         navLinks.forEach(links => {
           links.classList.remove('active');
           document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
-        })
-      }
-    })
+        });
+      };  
+    });
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -33,7 +34,7 @@ document.querySelector('#search-icon').onclick = () => {
 }
 
 document.querySelector('#close').onclick = () => {
-    document.querySelector('#search-form').classList.toggle('active');
+    document.querySelector('#search-form').classList.remove('active');
 }
 
 var swiper = new Swiper(".home-slider", {
