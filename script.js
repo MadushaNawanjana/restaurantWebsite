1153fbdb1e440e93dc5e480628cd4e6a17d30ce0
Madusha Nawanjana <95086533+MadushaNawanjana@users.noreply.github.com>
2023-05-29 22:39:17 +0530

    second commit

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ window.onscroll = () => {
       let height = sec.offsetHeight;
       let offset = sec.offsetTop - 150;
       let id = sec.getAttribute('id');
+
+      if (top => offset && top < offset + height){
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        })
+      }
     })
 }
 
