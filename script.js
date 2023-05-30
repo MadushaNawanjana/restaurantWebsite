c66a7bb71bbed1bd584514f55d0f5ad26065302e
Madusha Nawanjana <95086533+MadushaNawanjana@users.noreply.github.com>
2023-05-30 12:27:53 +0530

    added loader section

--- script.js
+++ script.js
@@ -6,36 +6,28 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+
+
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () => {
-      
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+  
+  let currentSectionIndex = 0;
+  section.forEach((sec, index) => {
+    const top = window.scrollY;
+    const height = sec.offsetHeight;
+    const offset = sec.offsetTop - 150;
+    
+    if (top >= offset && top < offset + height) {
+      currentSectionIndex = index;
+    }
+  });
+  
+  navLinks.forEach(link => link.classList.remove('active'));
+  navLinks[currentSectionIndex].classList.add('active');
+};
 
-    section.forEach(sec =>{
-      let top = window.scrollY;
-      let height = sec.offsetHeight;
-      let offset = sec.offsetTop - 150;
-      let id = sec.getAttribute('id');
-
-      if (top => offset && top < offset + height){
-        navLinks.forEach(links => {
-          links.classList.remove('active');
-          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
-        });
-      };  
-    });
-}
-
-document.querySelector('#search-icon').onclick = () => {
-    document.querySelector('#search-form').classList.toggle('active');
-}
-
-document.querySelector('#close').onclick = () => {
-    document.querySelector('#search-form').classList.remove('active');
-}
 
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
@@ -61,3 +53,23 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true,
   });
+
+  
+
+   function loader() {
+     document.querySelector('.loader-container').classList.add('fade-out');
+   }
+  
+   function fadeOut() {
+     setTimeout(() => {
+       loader();
+       // After the loader animation finishes, show the website content
+       setTimeout(() => {
+         document.querySelector('.loader-container').style.display = 'none';
+         document.querySelector('body').style.overflow = 'auto';
+       }, 2000); // Wait for 2 seconds after the loader animation
+     }, 1000); // Wait for 1 second before starting the loader animation
+   }
+  
+   window.onload = fadeOut;
+  
